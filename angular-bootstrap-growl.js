ef086a089b17a59720545c51087efb4e8d32bacc
danicomas <danicomas@gmail.com>
2014-09-04 12:16:58 +0200

    Added JavaScripts

---
+++ angular-bootstrap-growl.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+angular.module('angular-bootstrap-growl', []).factory('bootstrapGrowl', [function () {
+
+    var factory = function (content, options) {
+        $.growl(content, options);
+    };
+
+    return factory;
+}]).provider('$bootstrapGrowl', function () {
+    return {
+        default_options: $.growl.default_options,
+        $get: function () {
+            return {
+
+            }
+        }
+    }
+});
